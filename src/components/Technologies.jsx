f57a1570623be9da7fa4a28d0b1c86715ac93da2
Michael Medina <michaelmedina913@gmail.com>
2024-07-22 14:23:51 +0800

    first commit

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,101 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaPhp } from "react-icons/fa6";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa6";
+import { FaBootstrap } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useTransition } from "react";
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y:[10,-10],
+        transition:{
+            duration: duration,
+            ease:"linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+
+        }
+    
+    },
+});
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+        <motion.h1 
+        whileInView={{ opacity: 1,y:0}}
+        initial={{ opacity: 0,y: -100}}
+        transition={{ duration: 1.5}}
+        className="my-20 text-center text-4xl">Technologies
+        </motion.h1>
+
+        <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4">
+
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaHtml5 className="text-7xl text-orange-600 w-20 h-20"/>
+            </motion.div>
+
+            <motion.div 
+            variants={iconVariants(1.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaCss3Alt className="text-7xl text-blue-500 w-20 h-20"/>
+            </motion.div>
+
+            <motion.div 
+            variants={iconVariants(1)}
+            initial="initial"
+            animate="animate"className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaBootstrap className="text-7xl text-purple-500 w-20 h-20"/>
+            </motion.div>
+
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <IoLogoJavascript className="text-7xl text-yellow-400 border-radius-10 w-20 h-20"/>
+            </motion.div>
+
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4 ">
+                <RiReactjsLine className="text-7xl text-cyan-400 w-20 h-20"/>
+            </motion.div>
+
+            <motion.div 
+            variants={iconVariants(1)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaPhp className="text-7xl text-violet-400 w-20 h-20"/>
+            </motion.div>
+
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <svg className="w-20 h-20"xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48">
+                    <path fill="#0277bd" d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22	h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104	c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672	C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498	c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"></path><path fill="#ffc107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343	h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104	c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672	C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498	c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"></path>
+                </svg>
+            </motion.div>
+        </motion.div>
+    </div>
+  )
+}
+
+export default Technologies
---
+++ src/constants/index.js
@@ -0,0 +1,73 @@
+import project1 from "../assets/CALRSLogin.png";
+import project2 from "../assets/LIRCPortal.png";
+import project3 from "../assets/Portfolio.png";
+
+
+import experience1  from "../assets/Continental.png";
+import experience2  from "../assets/CCC.png";
+
+export const HERO_CONTENT = `I am a fresh graduate with a Bachelor of Science in Information Technology, eager to expand my knowledge and expertise in web development. I have hands-on experience with HTML, CSS, JavaScript, PHP, Bootstrap, Python, and React. I am passionate about building dynamic and responsive websites. To deliver innovative, high-quality solutions, I am committed to continuous learning and staying current with industry trends. Explore my portfolio to see the diverse projects I’ve worked on and my technical expertise in action.`;
+
+export const ABOUT_TEXT = `I am a fresh graduate with a Bachelor of Science in Information Technology, eager to expand my knowledge and expertise in web development. I have hands-on experience with HTML, CSS, JavaScript, PHP, Bootstrap, Python, and React. I am passionate about building dynamic and responsive websites, and I look forward to enhancing my skills and contributing to innovative web projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024",
+    image: experience1,
+    role: "IT Support Intern",
+    company: " Continental Calamba",
+    description: `Maintaining and implementing services inside and outside of the production and Server Rooms (Server Tracing)
+,Assisting Technical Supports (File transfer, Fixing software and hardware problems),
+Formatting laptops and desktops and setting up before deploying to the employees,
+Maintaining the Inventory of the IT Office,
+Maintaining and fixing printer bugs and problems`,
+    technologies: ["Software and Hardware Technical Support", "Networking"],
+  },
+  {
+    year: "2024",
+    image: experience2,
+    role: "Software Developer Intern",
+    company: " LIRC at City College of Calamba",
+    description: `Developed Two System for the CCC Library (LIRC)
+| LIRC Barcode Logging System
+| LIRC Portal`,
+    technologies: ["HTML", "CSS", "JavaScript", "Php","Bootstrap", "Ajax" ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Capstone Project: Credit Application and Loan Recording System",
+    image: project1,
+    description:
+      "The system focused on streamlining the credit application and making it simpler, faster, accessible, and more transparent for the applicants, reducing the use of a huge amount of paper and manual processes. also automating the recording procedure and monitoring loan transactions by enhancing the efficiency and accuracy of the loan recording system.",
+    technologies: ["HTML", "CSS", "JavaScript", "Php","Bootstrap", "Ajax"],
+  },
+  {
+    title: "LIRC Portal",
+    image: project2,
+    description:
+      "The system was implemented to maximize the student's capability to access LIRC services. The system itself has the feature of admin access to set an event or announcement anytime and anywhere because the system is designed to give online services to students and faculty staff.",
+    technologies: ["HTML", "CSS", "JavaScript", "Php","Bootstrap", "Ajax"],
+  },
+  {
+    title: "LIRC Barcode Logging System",
+    image: project2,
+    description:
+      "The system was developed to enhance the efficiency and security of accessing educational resources within the CCC Learning Information Resource Center. It's also designed to reduce the time spent on manual login procedures through the automated barcode system, improving overall operational efficiency.",
+    technologies: ["HTML", "CSS", "JavaScript", "Php","Bootstrap", "Ajax"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "My personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "JavaScript" ,"React" ],
+  },
+];
+
+export const CONTACT = {
+  address: "Michael Angelo Medina",
+  phoneNo: "0969-392-5786",
+  email: "michaelmedina913@gmail.com",
+};
